f4dc1f7d3d04510a77fd3e4779b4a29768d97342
Shubham Pathak <pathakshubham28@gmail.com>
2015-02-19 17:34:34 +0530

    Demo Learning Commit - 1

---
+++ BufferExample/src/BufferExample1.java
@@ -0,0 +1,30 @@
+import java.nio.CharBuffer;
+
+public class BufferExample1 {
+
+    public static void main (String [] args) {
+	
+        CharBuffer buffer = CharBuffer.allocate(12);
+
+        String text = "appleubju";
+        System.out.println("Input text: " + text);
+
+        for (int i = 0; i < text.length(); i++) {
+
+            char c = text.charAt(i);
+            buffer.put(c);
+        }
+
+        int buffPos = buffer.position();
+        System.out.println("Position after data is written into buffer: " + buffPos);
+
+        buffer.flip();
+		
+        System.out.println("Reading buffer contents:");
+		
+        while (buffer.hasRemaining()) {
+
+            System.out.println(buffer.get());                   
+        }
+    }
+}
